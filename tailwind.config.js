66ec5eaef94216791370bbcedf1b6e06e7501cdb
basilgoodluck <basilgoodluck22@gmail.com>
2024-08-17 01:57:54 +0100

    setup complete

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "gray-10": "#d5d8e8",
+        "gray-50": "#b6bad1",
+        "gray-300": "#9196b5",
+        "gray-500": "#525a85",
+        "primary-100": "#313b6e",
+        "primary-300": "#2c3359",
+        "primary-500": "#090f2e",
+        "secondary-100": "#db53d0",
+        "secondary-300": "#b52faa",
+        
+      },
+      backgroundImage: {
+
+      },
+      content: {
+
+      }
+    },
+    screens: {
+
+    }
+  },
+  plugins: [],
+}
