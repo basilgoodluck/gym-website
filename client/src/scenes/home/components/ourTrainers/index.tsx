0216ad7372609643e26ec58fd63c1908ba782f52
basilgoodluck <basilgoodluck22@gmail.com>
2024-09-08 01:43:16 +0100

    Updated files and deleted some images

--- client/src/scenes/home/components/ourTrainers/index.tsx
+++ client/src/scenes/home/components/ourTrainers/index.tsx
@@ -1,6 +1,6 @@
-import davidImage from "@/assets/David Nicholas.jpg";
-import owoichofavour from "@/assets/Owoicho Favour.jpg"
-import alhaji from "@/assets/Alhaji Ibraheem.jpg"
+import redMan from "@/assets/redMan.jpg";
+import pinkLady from "@/assets/pinkLady.jpg"
+import chestMan from "@/assets/chestMan.jpg"
 
 
 type Props = {}
@@ -13,7 +13,7 @@ const OurTrainers = ({}: Props) => {
             <p className="text-center text-gray-500 text-sm font-thin">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio sit labore mollitia, dolore cupiditate illo est eius eveniet</p>
             <div className="flex flex-col gap-6">
                 <div className="h-[500px] relative drop-shadow-2xl">
-                    <img src={davidImage} alt="mansd" className="w-full h-full object-cover" />
+                    <img src={redMan} alt="mansd" className="w-full h-full object-cover" />
                     <div className="absolute top-0 text-center bg-lightBlue h-full w-full flex justify-center items-center text-white flex-col gap-3 ">
                       <h4>David Nicholas</h4>
                       <p className="w-3/5 mx-auto text-sm">Hello guys! I am a David, it's a pleasure to connect with you all here.</p>
@@ -23,17 +23,17 @@ const OurTrainers = ({}: Props) => {
                     </button> */}
                 </div>
                 <div className="h-[500px] relative drop-shadow-2xl">
-                    <img src={owoichofavour} alt="mansd" className="w-full h-full object-cover" />
+                    <img src={pinkLady} alt="mansd" className="w-full h-full object-cover" />
                     <div className="absolute top-0 text-center bg-lightBlue h-full w-full flex justify-center items-center text-white flex-col gap-3 ">
-                      <h4>Owoicho Favour</h4>
-                      <p className="w-3/5 mx-auto text-sm">Hello guys! I am a David, it's a pleasure to connect with you all here.</p>
+                      <h4>Alhaja Miami</h4>
+                      <p className="w-3/5 mx-auto text-sm">Hello guys! I am a Miami, it's a pleasure to connect with you all here.</p>
                     </div>
                     {/* <button className="absolute bottom-7 bg-white w-3/5 p-2">
                       See details
                     </button> */}
                 </div>
                 <div className="h-[500px] relative drop-shadow-2xl">
-                    <img src={alhaji} alt="mansd" className="w-full h-full object-cover" />
+                    <img src={chestMan} alt="mansd" className="w-full h-full object-cover" />
                     <div className="absolute top-0 text-center bg-lightBlue h-full w-full flex justify-center items-center text-white flex-col gap-3 ">
                       <h4>Alhaji Ibraheem</h4>
                       <p className="w-3/5 mx-auto text-sm">Hello guys! I am a David, it's a pleasure to connect with you all here.</p>
