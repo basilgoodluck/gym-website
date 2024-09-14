212d1bfc3885ec34288ea6b6d754fe63abbf93cc
basilgoodluck <basilgoodluck22@gmail.com>
2024-09-14 01:27:47 +0100

    updated

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require("express")
-<<<<<<< HEAD
 const mongoose = require("mongoose")
 const cors = require("cors")
 const dotenv = require("dotenv")
@@ -19,10 +18,4 @@ const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log("Server is running")
-=======
-const app = express()
-
-app.get('/', (req, res) => {
-    res.send("Hello world")
->>>>>>> 8d4ed90e244f797d23bdd6244e46114a4d7a9b2d
 })
